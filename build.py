8894499ab7480ad50d0487ddff23beaa0a912178
AoD314 <morozov.andrey.vmk@gmail.com>
2011-03-06 03:26:55 +0300

    Реализовал разбор главной страницы.

--- build.py
+++ build.py
@@ -1,20 +1,33 @@
 #!/usr/bin/python3 
 
 from urllib.request import urlopen
-import re
+import re, io
 
 def main():
-	u = urlopen('http://www.calculate-linux.ru/main/ru/documentation')
-	text = u.read().decode('utf-8')
-	pattern = re.compile('<li><a href="/main/ru.*" class="wiki-page">.*</a></li>')
-	p_name  = re.compile('>.*</a>')
+	#u = urlopen('http://www.calculate-linux.ru/main/ru/documentation')
+	#text = u.read().decode('utf-8')
+	f = io.open('doc.tmp', 'r')
+	text = f.read().replace('<a href', '\n<a href')
+
+	pattern = re.compile('<a href="/main/ru.*" class="wiki-page">.*</a></li>')
 	r = re.findall(pattern, text)
 
-	print('found: ' + str(len(r)) + '\n')
+	llink = []
+
 	for s in r:
-		pos = s.find('page">')
-		print (s[pos+6:-9])
-		
+		b = 'wiki-page">'
+		pb = s.find(b)
+		pe = s.find('</a>')
+		title = s[pb+len(b):pe]
+		b = '<a href="'
+		pb = s.find(b)
+		pe = s.find('"', pb + len(b))
+		link  = 'http://www.calculate-linux.ru' + s[pb+len(b):pe]
+		llink = llink + [link]
+		print ( title + ': {' + link + '}')
+	
+	print ('total : ' + str(len(llink)))
+
 
 
 if __name__ == "__main__":
