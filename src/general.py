83e2a58d81095a40884277af5a597b044386440e
Andrey Morozov <morozov.andrey.vmk@gmail.com>
2015-12-20 23:38:18 +0300

    27

--- src/general.py
+++ src/general.py
@@ -36,7 +36,7 @@ def run_command(cmd):
         line = proc.stdout.readline()
         if not line: break
 
-        line = line.strip().decode()
+        line = line.decode('utf-8').strip()
         print(line)
 
     status = proc.returncode
