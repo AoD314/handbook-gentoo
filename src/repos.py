3d7eb1df042c10b0459397a7e156594a68115744
Andrey Morozov <morozov.andrey.vmk@gmail.com>
2016-01-06 01:02:47 +0300

    Update repos.py

--- src/repos.py
+++ src/repos.py
@@ -4,9 +4,14 @@ from general import create_file
 def configure():
     with create_file('/etc/portage/repos.conf/gentoo.conf') as f:
         print("""
+[DEFAULT]
+main-repo = gentoo
+
 [gentoo]
-location=/usr/portage
-sync-type=git
-sync-uri=https://github.com/gentoo-mirror/gentoo
-auto-sync=true
+location = /usr/portage
+sync-type = git
+sync-uri = git://anongit.gentoo.org/repo/gentoo.git
+auto-sync = yes
+sync-depth = 1 
+
 """, file=f)
