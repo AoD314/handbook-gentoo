efa1cdbc2c49af765a6fcf99ea84857adcea6c72
Andrey Morozov <morozov.andrey.vmk@gmail.com>
2016-01-06 01:01:54 +0300

    Update sysctl.py

--- src/sysctl.py
+++ src/sysctl.py
@@ -8,8 +8,8 @@ net.ipv4.ip_forward = 0
 net.ipv4.conf.default.rp_filter = 1
 net.ipv4.conf.all.rp_filter = 1
 
-vm.dirty_bytes = 67108864              # 64 MB
-vm.dirty_background_bytes = 67108864   # 64 MB
+vm.dirty_bytes = 67108864
+vm.dirty_background_bytes = 67108864
 
 vm.swappiness=0
 """, file=f)
