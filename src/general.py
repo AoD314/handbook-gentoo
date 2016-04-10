fe50c112e24f6a0aa57e0df084ab8e1be38774f7
Andrey Morozov <morozov.andrey.vmk@gmail.com>
2016-04-10 18:30:46 +0300

    123

--- src/general.py
+++ src/general.py
@@ -34,7 +34,7 @@ def run_command(cmd):
 def print_log(msg):
     print(msg, end='')
     sys.stdout.flush()
-    with open('/tmp/gentoo_install.log', a) as f:
+    with open('/tmp/gentoo_install.log', 'a') as f:
         f.write(str(datetime.now()) + ": " + str(msg))
 
 
