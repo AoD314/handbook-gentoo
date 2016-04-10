6a16d6860f0fff1978da7dd5ca20f506c3411605
Andrey Morozov <morozov.andrey.vmk@gmail.com>
2016-04-10 21:22:03 +0300

    Update network.py

--- src/network.py
+++ src/network.py
@@ -1,4 +1,4 @@
-from general import create_file
+from general import create_file, run_command
 
 import os
 
@@ -11,6 +11,8 @@ def configure():
     with create_file('/etc/conf.d/net') as f:
         print('config_eth0="dhcp"', file=f)
         print('config_eth1="dhcp"', file=f)
+        
+    run_command("emerge -v1 net-misc/dhcpcd")
 
     os.chdir('/etc/init.d')
     os.system('ln -s net.lo net.eth0')
