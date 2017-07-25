fbfbe74d6fb47d971c52e608afa6b1c6a4e28e7c
Andrey Morozov <morozov.andrey.vmk@gmail.com>
2017-07-25 22:40:23 +0300

    Update sysctl.py

--- deploy/files/sysctl.py
+++ deploy/files/sysctl.py
@@ -13,6 +13,9 @@ net.ipv4.ip_forward = 0
 net.ipv4.conf.default.rp_filter = 1
 net.ipv4.conf.all.rp_filter = 1
 
+vm.dirty_bytes = 67108864
+vm.dirty_background_bytes = 50331648
+
 vm.swappiness=0
 """, file=f)
 
