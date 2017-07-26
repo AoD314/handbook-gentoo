0fa429330f49c8dbdc96a882ba4bd649ae2ba91a
Andrey Morozov <morozov.andrey.vmk@gmail.com>
2017-07-26 08:27:23 +0300

    Update sysctl.py

--- deploy/files/sysctl.py
+++ deploy/files/sysctl.py
@@ -27,7 +27,7 @@ net.core.wmem_max = 16777216
 # максимальное кол-во соединений
 net.ipv4.tcp_max_orphans = 65536
 
-net.ipv4.tcp_mem=50576 64768 98152
+net.ipv4.tcp_mem=65536 131072 262144
 net.ipv4.tcp_rmem=4096 87380 16777216
 net.ipv4.tcp_wmem=4096 65536 16777216
 
