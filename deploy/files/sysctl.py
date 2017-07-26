0b20319d14a71a6fef9c9f51e8810140b9c0d356
Andrey Morozov <morozov.andrey.vmk@gmail.com>
2017-07-26 08:16:22 +0300

    Update sysctl.py

--- deploy/files/sysctl.py
+++ deploy/files/sysctl.py
@@ -16,6 +16,34 @@ net.ipv4.conf.all.rp_filter = 1
 vm.dirty_bytes = 67108864
 vm.dirty_background_bytes = 50331648
 
+# Устанавливаем в 256 Кб размер буферов по умолчанию для приема и отправки данных через сокеты
+net.core.rmem_default = 262144
+net.core.wmem_default = 262144
+
+# Устанавливаем в 8Мб максимальный размер буфера сокетов.
+net.core.rmem_max = 8388608
+net.core.wmem_max = 8388608
+
+# максимальное кол-во соединений
+net.ipv4.tcp_max_orphans = 65536
+
+net.ipv4.tcp_mem=50576 64768 98152
+net.ipv4.tcp_rmem=4096 87380 16777216
+net.ipv4.tcp_wmem=4096 65536 16777216
+
+net.ipv4.tcp_sack = 1
+net.ipv4.tcp_no_metrics_save = 1
+
+net.ipv4.tcp_fin_timeout = 10
+
+# Максимальный размер очереди 
+net.core.netdev_max_backlog = 2048
+
+net.core.rmem_default = 65536
+net.core.wmem_default = 65536
+net.core.rmem_max = 16777216
+net.core.wmem_max = 16777216
+
 vm.swappiness=0
 """, file=f)
 
